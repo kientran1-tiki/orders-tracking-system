54a4d23431c057ac7a95ef80d8a557f885920a76
KeenKnif147 <kien.tran1@tiki.vn>
2022-12-10 15:13:44 +0700

    Updates

--- src/pages/OrderDetail/thunk.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
-export const changeOrderStatus = createAsyncThunk(
-  "order/changeOrderStatus",
-  async (obj: any) => {
-    return obj;
-  }
-);
